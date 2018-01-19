3c9114401c02f558723e6f8e8fb62385a2e3586f
root <root@localhost.localdomain>
2018-01-18 21:47:20 -0500

    This is my first

---
+++ fruit_basket.py
@@ -0,0 +1,24 @@
+fruit_basket = ["Strawberries", "Grapes", "Apples", "Bananas", "Watermelon"]
+
+count = 1
+While count <6
+
+guess = raw_input("Guess a Fruit")
+
+if guess = ("Strawberries"):
+	print("That is Correct")
+
+elif guess = ("Grapes"):
+	print("That is correct")
+
+elif guess = ("Apples"):
+	print("That is correct")
+
+elif guess = ("Bananas"):
+	print("That is correct")
+
+elif guess = ("Watermelon"):
+	print("That is correct")
+
+
+
